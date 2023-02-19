78f901a83e34b91f86d9031b8333f1c32902609c
rania <raniahamada0308@gmail.com>
2023-02-19 22:34:44 +0200

    print alphaabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'print alphabets'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 97;
+	int i = 123;
+
+	while (ch < i)
+	{
+		putchar(ch);
+		ch ++;
+	}
+	putchar('\n');
+
+	return (0);
+}
