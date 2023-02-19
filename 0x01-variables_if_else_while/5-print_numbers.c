42f0db1738ecaf680a75303d92a6204e4bae45f9
rania <raniahamada0308@gmail.com>
2023-02-19 23:05:02 +0200

    print digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print decimal digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+	while (n < 10)
+	{
+		printf("%i", n);
+		n++;
+	}
+	printf("\n");
+	return (0);	
+}
