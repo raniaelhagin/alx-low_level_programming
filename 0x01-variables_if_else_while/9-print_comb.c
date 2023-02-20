db0488608fefd2115b1f6393775015472f2ba936
rania <raniahamada0308@gmail.com>
2023-02-20 17:36:40 +0200

    print numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'print all possible 
+ * combinations of single-digit numbers'
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	i = 48;
+	while (i < 58)
+	{
+		putchar(i);
+		if (i < 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
