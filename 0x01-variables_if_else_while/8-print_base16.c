173fb6bb64944d8ea0d4825ccbd387d47d4a8177
rania <raniahamada0308@gmail.com>
2023-02-20 17:16:24 +0200

    print base 16 digits

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'print base 16 digits'
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	
+	i = 48;
+	while(i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	
+	i = 97;
+	while(i < 103)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
