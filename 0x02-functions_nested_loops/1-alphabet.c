48002f867ba5ae5cd6e56747854640a95f635802
rania <raniahamada0308@gmail.com>
2023-02-22 19:52:11 +0200

    print alphbets using _putchar function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: print alphabets in lower case
+ * using _putchar function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 97;
+
+	while(i < 123)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
