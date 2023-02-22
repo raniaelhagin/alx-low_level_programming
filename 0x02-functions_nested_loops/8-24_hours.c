207725ff79bbc957d1943cc5b5116b489ea436a6
rania <raniahamada0308@gmail.com>
2023-02-22 22:13:40 +0200

    print minutes of a day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+ * jack_bauer - prints every minute of the day of Jack Bauer,
+ * starting from 00:00 to 23:59
  *
  * Return: None
  */
@@ -14,11 +15,11 @@ void jack_bauer(void)
 	{
 		while (j < 60)
 		{
-			_putchar(i/10 + 48);
-			_putchar(i%10 + 48);
+			_putchar(i / 10 + 48);
+			_putchar(i % 10 + 48);
 			_putchar(':');
-			_putchar(j/10 + 48);
-			_putchar(j%10 + 48);
+			_putchar(j / 10 + 48);
+			_putchar(j % 10 + 48);
 			_putchar('\n');
 			j++;
 		}
