d88d49349cc5efd795b66bbbc5149e8f3f41a929
rania <raniahamada0308@gmail.com>
2023-02-22 19:11:27 +0200

    print _putchar using _putchar function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,22 +3,22 @@
 
 /**
  * main - Entry point 
- * 
+ *
  * Description: print _putchar using _putchar
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	_putchar('_');
-        _putchar('p');
+       	_putchar('p');
 	_putchar('u');
-        _putchar('t');
-        _putchar('c');
-        _putchar('h');
-        _putchar('a');
-        _putchar('r');
-        _putchar('\n');
-    
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	
 	return (0);
 }
