08f00711620acac6a528fd06a68d359910d07cae
rania <raniahamada0308@gmail.com>
2023-02-24 01:49:07 +0200

    print numbers from 0 to 14 using only 3 _putchar calls

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,22 +7,22 @@
  */
 void more_numbers(void)
 {
-	int i = 0;
-	int j = 0;
+	int i, j;
 	
+	j = 0;
 	while (j < 10)
 	{
+		i = 0;
 		while (i < 15)
 		{
 			if (i >= 10)
 			{
-				_putchar(i / 10 + '0');
+				_putchar((i / 10) + '0');
 			}
-			_putchar(i % 10 + '0');
+			_putchar((i % 10) + '0');
 			i++;
 		}
 		_putchar('\n');
 		j++;
-		i = 0;
 	}
 }
