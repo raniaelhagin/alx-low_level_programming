cb725c6892fab9e5ec8d49227a8051e7fe8547d0
rania <raniahamada0308@gmail.com>
2023-02-24 00:41:51 +0200

    check polarity

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -5,22 +5,22 @@
 
 /**
  * positive_or_negative - check if the number is +ve or -ve or 0
- * @n: the integer
+ * @i: the integer
  *
  * Return: Always 0 (Success)
  */
-void positive_or_negative(int n)
+void positive_or_negative(int i)
 {
-	if (n == 0)
+	if (n == i)
 	{
-		printf("%d is zero\n", n);
+		printf("%d is zero\n", i);
 	}
-	else if (n > 0)
+	else if (i > 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%d is positive\n", i);
 	}
 	else
 	{
-		printf("%d is negative\n", n);
+		printf("%d is negative\n", i);
 	}
 }
