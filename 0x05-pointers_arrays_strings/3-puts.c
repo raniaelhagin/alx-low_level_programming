3c67a96bae3d967c9aac99b44b10e427a3369771
rania <raniahamada0308@gmail.com>
2023-02-27 18:19:32 +0200

    print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line
+ * @str: the string to be printed
+ *
+ * Return: None
+ */
+void _puts(char *str)
+{
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
