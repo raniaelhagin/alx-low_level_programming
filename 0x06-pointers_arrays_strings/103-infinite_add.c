09ce661e846c314e6cff86f0fe248e87f00caf68
rania <raniahamada0308@gmail.com>
2023-03-04 01:16:32 +0200

    add two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -45,7 +45,7 @@ int get_val(char *s, int index)
  * @n2: the second number
  * @r: the buffer used to store the result
  * @size_r: the buffer size
- * 
+ *
  * Return: a pointer to the result
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
