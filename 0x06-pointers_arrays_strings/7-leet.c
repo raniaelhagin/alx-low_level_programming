e95015bd586fee7134ae7c698e55b7604483d5a9
rania <raniahamada0308@gmail.com>
2023-03-03 12:16:28 +0200

    encode a string to 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,29 +9,21 @@
 char *leet(char *s)
 {
 	int i = 0;
+	int n = 0;
+	char codes[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int digits[5] = {4, 3, 0, 7, 1};
 
 	while (s[i] != '\0')
 	{
-		if (s[i] == 'a' || s[i] == 'A')
+		while (n < 10)
 		{
-			s[i] = 4 + '0';
+			if (s[i] == codes[n] || s[i] == codes[n + 1])
+			{
+				s[i] = digits[n / 2] + '0'; 
+			}
+			n += 2;
 		}
-		if (s[i] == 'e' || s[i] == 'E')
-		{
-			s[i] = 3 + '0';
-		}
-		if (s[i] == 'o' || s[i] == 'O')
-		{
-			s[i] = '0';
-		}
-		if (s[i] == 't' || s[i] == 'T')
-		{
-			s[i] = 7 + '0';
-		}
-		if (s[i] == 'l' || s[i] == 'L')
-		{
-			s[i] = 1 + '0';
-		}
+		n = 0;
 		i++;
 	}
 	return (s);
