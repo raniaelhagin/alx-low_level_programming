793601bc9299696b11d1135e4c794884dba3a10f
rania <raniahamada0308@gmail.com>
2023-03-03 04:32:43 +0200

    copy a string

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,6 +2,38 @@
 #include "_strlen.c"
 
 /**
+ * getDiff - a fn to get the difference in case the two strings
+ * don't have the same length
+ * @str1: the first string to be searched
+ * @str2: the second string
+ *
+ * Return: integer value for the diff
+ */
+int getDiff(char *str1, char *str2)
+{
+	int i = 0;
+	int j = 0;
+	int diff = 0;
+
+	while (str1[i] != '\0')
+	{
+		diff = (str1[i] - str2[j]);
+		if (diff != 0)
+		{
+			break;
+		}
+		i++;
+		j++;
+	}
+	if (diff == 0)
+	{
+		diff = (str1[i] - ' ');
+	}
+
+	return (diff);
+}
+
+/**
  * _strcmp - compares two strings, s1 and s2
  * @s1: the first string
  * @s2: the second string
@@ -17,8 +49,7 @@ int _strcmp(char *s1, char *s2)
 	int s1_len = _strlen(s1);
 	int s2_len = _strlen(s2);
 	int i = 0;
-	int j = 0;
-	
+
 	if (s1_len == s2_len)
 	{
 		while (s1[i] != '\0')
@@ -30,45 +61,18 @@ int _strcmp(char *s1, char *s2)
 			}
 			i++;
 		}
-		
+
 		return (diff);
 	}
 	else if (s1_len > s2_len)
 	{
-		while (s2[j] != '\0')
-		{
-			diff = (s1[i] - s2[j]);
-			if (diff != 0)
-			{
-				break;
-			}
-			i++;
-			j++;
-		}
-		if (diff == 0)
-		{
-			diff = (s1[i] - ' ');
-		}
+		diff = getDiff(s1, s2);
 
 		return (diff);
 	}
 	else
 	{
-		while (s1[i] != '\0')
-		{
-			diff = (s1[i] - s2[j]);
-			if (diff != 0)
-			{
-				break;
-			}
-			j++;
-			i++;
-		}
-		if (diff == 0)
-		{
-			diff = (' ' - s2[j]);
-		}
-		
+		diff = -1 * getDiff(s2, s1);
 		return (diff);
 	}
 }
